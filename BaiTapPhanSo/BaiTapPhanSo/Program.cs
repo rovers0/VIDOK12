c1b6fd58d67a84c13e0f2824f82b88bfd7c30157
ctphu <phu.t.cao@outlook.com>
2019-09-19 14:19:31 +0700

    BaiTapPhanSo

---
+++ BaiTapPhanSo/BaiTapPhanSo/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: C5-D01
+ * Date: 9/12/2019
+ * Time: 3:32 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BaiTapPhanSo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Console.WriteLine("Hello World!");
+			clsPhanSo a, b, c, d, e, f, g;
+			a = new clsPhanSo();
+			b = new clsPhanSo(1,6);
+			c = new clsPhanSo(1,3);
+			//d = clsPhanSo.Cong(b,c);
+			d = b + c;
+			e = clsPhanSo.Tru(b,c);
+			try
+			{
+				f = clsPhanSo.Chia(b,c);
+				f.In();
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+			
+			
+			a.In();
+			d.In();
+			e.In();
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ BaiTapPhanSo/BaiTapPhanSo/clsPhanSo.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: C5-D01
+ * Date: 9/12/2019
+ * Time: 3:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace BaiTapPhanSo
+{
+	/// <summary>
+	/// Description of clsPhanSo.
+	/// </summary>
+	public class clsPhanSo
+	{
+		public int iTuSo, iMauSo;
+		public static int USCLN(int a, int b)
+		{
+			a = Math.Abs(a);
+			b = Math.Abs(b);
+			if (a == b)
+				return a;
+			if (a > b)
+				return USCLN(b, a - b);
+			else
+				return USCLN(a, b - a);
+		}
+		public void ToiGian()
+		{
+			int i = USCLN(iTuSo, iMauSo);
+			iTuSo = iTuSo / i;
+			iMauSo = iMauSo / i;
+		}
+		public static clsPhanSo Cong(clsPhanSo pA, clsPhanSo pB)
+		{
+			clsPhanSo psC = new clsPhanSo();
+			psC.iTuSo = pA.iTuSo* pB.iMauSo + pA.iMauSo * pB.iTuSo;
+			psC.iMauSo = pA.iMauSo * pB.iMauSo;
+			psC.ToiGian();
+			return psC;
+		}
+		public static clsPhanSo Tru(clsPhanSo pA, clsPhanSo pB)
+		{
+			clsPhanSo psC = new clsPhanSo();
+			psC.iTuSo = pA.iTuSo* pB.iMauSo - pA.iMauSo * pB.iTuSo;
+			psC.iMauSo = pA.iMauSo * pB.iMauSo;
+			psC.ToiGian();
+			return psC;
+		}
+		public static clsPhanSo Nhan(clsPhanSo pA, clsPhanSo pB)
+		{
+			clsPhanSo psC = new clsPhanSo();
+			psC.iTuSo = pA.iTuSo* pB.iTuSo;
+			psC.iMauSo = pA.iMauSo * pB.iMauSo;
+			psC.ToiGian();
+			return psC;
+		}
+		public static clsPhanSo Chia(clsPhanSo pA, clsPhanSo pB)
+		{
+			if(pB.iTuSo == 0)
+			{
+				throw new Exception("Khong the chia cho 0");
+				return null;
+			}
+			clsPhanSo psC = new clsPhanSo();
+			psC.iTuSo = pA.iTuSo* pB.iMauSo;
+			psC.iMauSo = pA.iMauSo * pB.iTuSo;
+			psC.ToiGian();
+			return psC;
+		}
+		public static clsPhanSo operator +(clsPhanSo pA, clsPhanSo pB)
+		{
+			return clsPhanSo.Cong(pA, pB);
+		}
+		public void In()
+		{
+			Console.WriteLine("{0} / {1}" , this.iTuSo, this.iMauSo);
+		}
+		public clsPhanSo()
+		{
+			iTuSo = 0;
+			iMauSo = 1;
+		}
+		public clsPhanSo(int piTuSo, int piMauSo)
+		{
+			if(piMauSo == 0)
+			{
+				iTuSo = 0;
+				iMauSo = 1;
+			}
+			else
+			{
+				iTuSo = piTuSo;
+				iMauSo = piMauSo;
+			}
+		}
+		public clsPhanSo(clsPhanSo pA)
+		{
+			this.iTuSo = pA.iTuSo;
+			this.iMauSo = pA.iMauSo;
+		}
+	}
+}
