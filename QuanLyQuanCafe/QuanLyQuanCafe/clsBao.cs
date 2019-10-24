05c03b616bff88837aba1e743bb6c580deb981b5
ctphu <phu.t.cao@outlook.com>
2019-10-24 16:18:41 +0700

    Add Hash Password

---
+++ QuanLyQuanCafe/QuanLyQuanCafe/clsBao.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VienDong
+ * Date: 10/24/2019
+ * Time: 2:53 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace QuanLyQuanCafe
+{
+	/// <summary>
+	/// Description of clsBao.
+	/// </summary>
+	public class clsBao
+	{
+		public clsBao()
+		{
+		}
+	}
+}
